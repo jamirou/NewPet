5ea44183a2862bcf8e3716e64620c6c224614ca1
jamirou <devjamiro@gmail.com>
2023-11-22 18:26:56 -0300

    component 2 event1

--- app/src/main/java/com/jamirodev/mynewpet/ui/UserInputViewModel.kt
+++ app/src/main/java/com/jamirodev/mynewpet/ui/UserInputViewModel.kt
@@ -1,5 +1,6 @@
 package com.jamirodev.mynewpet.ui
 
+import android.util.Log
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import com.jamirodev.mynewpet.data.UserDataUiEvents
@@ -18,11 +19,15 @@ class UserInputViewModel : ViewModel() {
                 uiState.value = uiState.value.copy(
                     nameEntered = event.name
                 )
+                Log.d(TAG, "onEvent:UserNameEntered->>")
+                Log.d(TAG, "${uiState.value}")
             }
             is UserDataUiEvents.AnimalSelected -> {
                 uiState.value = uiState.value.copy(
                     animalSelected = event.animalValue
                 )
+                Log.d(TAG, "onEvent:AnimalSelected->>")
+                Log.d(TAG, "${uiState.value}")
             }
         }
     }

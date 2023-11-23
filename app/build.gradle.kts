dee80215d6dedf2d35e9bde3db90b27e6bfac055
jamirou <devjamiro@gmail.com>
2023-11-22 23:34:39 -0300

    change theme

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -70,4 +70,5 @@ dependencies {
     //Navigation
     implementation("androidx.navigation:navigation-compose:2.7.0")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.4.1")
+    implementation("androidx.core:core-splashscreen:1.0.1")
 }
